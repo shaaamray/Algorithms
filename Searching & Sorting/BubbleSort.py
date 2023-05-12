8c9be4c85b1c3e3eeb0b3953e5a19509d4d4aa7a
shaaamray <83807318+shaaamray@users.noreply.github.com>
2023-05-13 05:02:17 +0600

    Add files via upload

---
+++ Searching & Sorting/BubbleSort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [5, 1, 4, 2, 8]
+bubbleSort(arr)
+
+print("Sorted array:", arr)
